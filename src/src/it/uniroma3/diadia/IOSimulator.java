3c5f37ee6c7b09a36a5049cdba8feba8abc67257
mat.dipangrazio@stud.uniroma3.it <mat.dipangrazio@stud.uniroma3.it>
2023-05-08 13:36:05 +0200

    prova

---
+++ src/src/it/uniroma3/diadia/IOSimulator.java
@@ -0,0 +1,43 @@
+package it.uniroma3.diadia;
+
+public class IOSimulator implements IO {
+
+	private String[] input;
+	private int contoInput;
+	private int contoIstruzioniConsumate;
+	private String[] output;
+	private int contoOutput;
+	
+	public IOSimulator(String[] istruzioni) {
+		this.input = istruzioni;
+		this.contoInput = istruzioni.length;
+		this.contoIstruzioniConsumate = 0;
+		this.output = new String[this.contoInput + 1];
+		this.contoOutput = 0;
+	}
+	
+	@Override
+	public void mostraMessaggio(String messaggio) {
+		System.out.println(messaggio);
+		System.out.println("-------------------------------------------------------------------------");
+		this.output[this.contoOutput] = messaggio;
+		this.contoOutput++;
+	}
+
+	@Override
+	public String leggiRiga() {
+		String riga = "fine";
+		if(contoIstruzioniConsumate < contoInput) {
+			riga = this.input[this.contoIstruzioniConsumate];
+			this.contoIstruzioniConsumate++;
+		}
+		else {
+			this.mostraMessaggio("ISTRUZIONI FINITE --> PARTITA PERSA");
+		}
+		return riga;
+	}
+	
+	public String[] getOutput() {
+		return this.output;
+	}
+}
